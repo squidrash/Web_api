e53ef6d04eae2af9abafdd7fc7992a55a5d64b29
nikitarasevskij <squidrash777@gmail.com>
2021-11-02 15:30:49 +0300

    фикс проблемы мапинга клиента со всеми данными

--- Web_api_pizza/MappingProfile.cs
+++ Web_api_pizza/MappingProfile.cs
@@ -32,12 +32,37 @@ namespace Web_api_pizza
                 .ForMember(dto => dto.NumberOfBuild, opt => opt.MapFrom(en => en.Address.NumberOfBuild))
                 .ForMember(dto => dto.NumberOfEntrance, opt => opt.MapFrom(en => en.Address.NumberOfEntrance))
                 .ForMember(dto => dto.Apartment, opt => opt.MapFrom(en => en.Address.Apartment));
+
             CreateMap<CustomerOrderEntity, CustomerDTO>()
                 .ForMember(dto => dto.Id, opt => opt.MapFrom(en => en.Customer.Id))
                 .ForMember(dto => dto.Name, opt => opt.MapFrom(en => en.Customer.Name))
                 .ForMember(dto => dto.LastName, opt => opt.MapFrom(en => en.Customer.LastName))
                 .ForMember(dto => dto.Phone, opt => opt.MapFrom(en => en.Customer.Phone))
                 .ForMember(dto => dto.Discount, opt => opt.MapFrom(en => en.Customer.Discount));
+
+            CreateMap<CustomerOrderEntity, OrderDTO>()
+                .ForMember(dto => dto.Id, opt => opt.MapFrom(en => en.Order.Id))
+                .ForMember(dto => dto.Status, opt => opt.MapFrom(en => en.Order.Status))
+                .ForMember(dto => dto.CreatTime, opt => opt.MapFrom(en => en.Order.CreatTime))
+                .ForMember(dto => dto.TotalSum, opt => opt.MapFrom(en => en.Order.TotalSum))
+                .ForMember(dto => dto.Dishes, opt => opt.MapFrom(en => en.Order.Products));
+
+
+            CreateMap<CustomerAddressEntity, AddressDTO>()
+                .ForMember(dto => dto.Id, opt => opt.MapFrom(en => en.Address.Id))
+                .ForMember(dto => dto.City, opt => opt.MapFrom(en => en.Address.City))
+                .ForMember(dto => dto.Street, opt => opt.MapFrom(en => en.Address.Street))
+                .ForMember(dto => dto.NumberOfBuild, opt => opt.MapFrom(en => en.Address.NumberOfBuild))
+                .ForMember(dto => dto.NumberOfEntrance, opt => opt.MapFrom(en => en.Address.NumberOfEntrance))
+                .ForMember(dto => dto.Apartment, opt => opt.MapFrom(en => en.Address.Apartment));
+
+            CreateMap<CustomerAddressEntity, CustomerDTO>()
+                .ForMember(dto => dto.Id, opt => opt.MapFrom(en => en.Customer.Id))
+                .ForMember(dto => dto.Name, opt => opt.MapFrom(en => en.Customer.Name))
+                .ForMember(dto => dto.LastName, opt => opt.MapFrom(en => en.Customer.LastName))
+                .ForMember(dto => dto.Phone, opt => opt.MapFrom(en => en.Customer.Phone))
+                .ForMember(dto => dto.Discount, opt => opt.MapFrom(en => en.Customer.Discount));
+
 
 
 
